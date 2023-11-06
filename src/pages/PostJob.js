75c16d7d615775135f87109bc785996afbf234b2
Omar4GH <103641847+Omar4GH@users.noreply.github.com>
2023-11-06 23:11:06 +0300

    Updated Post Job Page. #4

--- src/pages/PostJob.js
+++ src/pages/PostJob.js
@@ -3,16 +3,15 @@ import _axios from "../api/_axios";
 import { AuthContext } from "../context/authContext";
 import { useNavigate } from "react-router-dom";
 
-import Input from '@mui/joy/Input';
+import Input from "@mui/joy/Input";
 import Autocomplete from "@mui/material/Autocomplete";
 import { TextField } from "@material-ui/core";
 
 const PostJob = () => {
-    const { currentUser } = useContext(AuthContext);
-    const navigate = useNavigate();
-    
-  const [err,setError] = useState(null);
+  const { currentUser } = useContext(AuthContext);
+  const navigate = useNavigate();
 
+  const [err, setError] = useState(null);
 
   const [countries, setCountries] = useState([]);
   const [selectedCountry, setSelectedCountry] = useState("");
@@ -28,20 +27,20 @@ const PostJob = () => {
       });
   }, []);
 
-
   const [formData, setFormData] = useState({
-    title: '',
-    description: '',
-    location: selectedCountry,
-    position: '',
-    tags: '', // Assuming tags are comma-separated
+    title: "",
+    description: "",
+    location: "",
+    position: "",
+    tags: [], // Assuming tags are comma-separated
+    active: true,
   });
 
   const jobData = {
     ...formData,
     postdate: new Date().toISOString(),
     likes: 0,
-    uid: currentUser._id,
+    ...(currentUser ? { uid: currentUser._id } : {}),
   };
 
   const handleCountryChange = (event, value) => {
@@ -52,32 +51,43 @@ const PostJob = () => {
     });
     console.log(formData);
   };
-  
+
   const handleInputChange = (e) => {
     const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
+  
+    // For the "tags" field, split the comma-separated values into an array
+    if (name === "tags") {
+      const tagsArray = value.split(" ").map((tag) => tag.trim());
+      setFormData({
+        ...formData,
+        [name]: tagsArray,
+      });
+    } else {
+      setFormData({
+        ...formData,
+        [name]: value,
+      });
+    }
     console.log(formData);
   };
 
+  const submit = async (e) => {
+    e.preventDefault();
+    try {
+      await _axios.post("/jobs/", jobData);
+      navigate("/feed");
+    } catch (err) {
+      setError(err.response.data);
+    }
+  };
 
-    const submit = async (e) => {
-        e.preventDefault();
-        try {
-          await _axios.post("/jobs/", jobData);
-          navigate("/profile");
-        } catch (err) {
-          setError(err.response.data);
-        }
-      };
-
-      return (
-        <div className="flex justify-center items-center h-screen">
-          <div className="w-2/3 justify-center items-center">
+  return (
+    <div className="flex justify-center bg-gray-100 items-center h-screen">
+      {currentUser ? (
+        <>
+          <div className="w-4/5 justify-center bg-white p-8 items-center rounded-3xl shadow-lg">
             <h1 className="text-2xl font-bold mb-4">Post a Job</h1>
-      
+
             <form onSubmit={submit}>
               <div className="mb-4">
                 <label htmlFor="jobTitle" className="block font-semibold mb-2">
@@ -88,15 +98,18 @@ const PostJob = () => {
                   id="title"
                   name="title"
                   placeholder="Enter Job Title"
-                  className="w-full p-2 border border-gray-300 rounded-md"
+                  className="w-1/2 p-2 border border-gray-300 rounded-md"
                   required
                   value={formData.title}
                   onChange={handleInputChange}
                 />
               </div>
-      
+
               <div className="mb-4">
-                <label htmlFor="description" className="block font-semibold mb-2">
+                <label
+                  htmlFor="description"
+                  className="block font-semibold mb-2"
+                >
                   Description
                 </label>
                 <textarea
@@ -104,34 +117,38 @@ const PostJob = () => {
                   name="description"
                   placeholder="Enter Job Description"
                   rows="4"
-                  className="w-full p-2 border border-gray-300 rounded-md"
+                  className="w-1/2 p-2 border border-gray-300 rounded-md"
                   required
                   value={formData.description}
                   onChange={handleInputChange}
                 />
               </div>
-      
+
               <div className="mb-4">
                 <label htmlFor="location" className="block font-semibold mb-2">
                   Location
                 </label>
-                <Autocomplete
-                disablePortal
-                id="location"
-                name="location"
-                options={countries}
-                sx={{ width: 250 }}
-                className="bg-white"
-                value={selectedCountry}
-                onChange={handleCountryChange}
-                renderInput={(params) => (
-                  <TextField {...params} label="Countries" />
-                )}
-              />
+                <select
+                  required // use a <select> element instead of <input> for gender
+                  name="location"
+                  className="block w-1/3 px-4 py-2 mt-2 text-slate-700 bg-white border rounded-md focus:border-slate-700 focus:ring-slate-700 focus:outline-none focus:ring focus:ring-opacity-40"
+                  onChange={handleInputChange}
+                  id="location"
+                >
+                  <option value="">-- Select Country --</option>
+                  {countries.map((countries) => (
+                    <option key={countries} value={countries}>
+                      {countries}
+                    </option>
+                  ))}
+                </select>
               </div>
-      
+
               <div className="mb-4">
-                <label htmlFor="jobPosition" className="block font-semibold mb-2">
+                <label
+                  htmlFor="jobPosition"
+                  className="block font-semibold mb-2"
+                >
                   Job Position
                 </label>
                 <Input
@@ -139,13 +156,13 @@ const PostJob = () => {
                   id="position"
                   name="position"
                   placeholder="Enter Job Position"
-                  className="w-full p-2 border border-gray-300 rounded-md"
+                  className="w-1/6 p-2 border border-gray-300 rounded-md"
                   required
                   value={formData.position}
                   onChange={handleInputChange}
                 />
               </div>
-      
+
               <div className="mb-4">
                 <label htmlFor="tags" className="block font-semibold mb-2">
                   Tags
@@ -155,21 +172,46 @@ const PostJob = () => {
                   id="tags"
                   name="tags"
                   placeholder="Enter Tags (comma-separated)"
-                  className="w-full p-2 border border-gray-300 rounded-md"
+                  className="w-1/5 p-2 border border-gray-300 rounded-md"
                   required
                   value={formData.tags}
                   onChange={handleInputChange}
                 />
               </div>
-      
-              <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded-md">
-                Submit Job
-              </button>
+              <div className="flex">
+                <select
+                  required
+                  name="active" // Assign the name attribute here
+                  className="block mx-5 px-4 py-2 mt-2 text-slate-700 bg-white border rounded-md focus:border-blue-700 focus:ring-blue-700 focus:outline-none focus:ring focus:ring-opacity-40"
+                  onChange={handleInputChange}
+                  id="active"
+                  sx={{ width: 250 }}
+                >
+                  <option value={true}>Public</option>
+                  <option value={false}>Private</option>
+                </select>
+                <button
+                  type="submit"
+                  className=" px-4 py-2  bg-blue-950 text-white transition-colors duration-200 transform rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-300"
+                >
+                  Submit Job
+                </button>
+              </div>
             </form>
           </div>
-        </div>
-      );
-      
+        </>
+      ) : (
+        <>
+          <div>
+            {" "}
+            <button className="w-full px-4 py-2 tracking-wide bg-blue-950 text-white transition-colors duration-200 transform rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-300 ">
+              <a href="/login">Login or Register to Post a Job</a>
+            </button>
+          </div>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default PostJob;
