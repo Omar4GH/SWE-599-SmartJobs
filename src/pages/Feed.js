55f899f4348bdea71e8792b94209cd4833a64200
Omar4GH <103641847+Omar4GH@users.noreply.github.com>
2023-11-24 05:26:25 +0300

    Added PostsColors+profilePics+titleSearch

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -4,8 +4,12 @@ import CardContent from "@mui/joy/CardContent";
 import Skeleton from "@mui/joy/Skeleton";
 import Typography from "@mui/material/Typography";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
-
+import TextField from "@mui/material/TextField";
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import "react-datepicker/dist/react-datepicker.css";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import PersonIcon from "@mui/icons-material/Person";
+import SearchIcon from "@material-ui/icons/Search";
 import {
   Avatar,
   Chip,
@@ -16,13 +20,22 @@ import {
 import _axios from "../api/_axios";
 import moment from "moment";
 import ThumbUpOffAlt from "@mui/icons-material/ThumbUpOffAlt";
+import CalendarIcon from "@mui/icons-material/CalendarMonth";
+import TagIcon from "@mui/icons-material/Tag";
 import ThumbUpIcon from "@mui/icons-material/ThumbUp";
 import CircularProgress from "@mui/material/CircularProgress";
 import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
 import BookmarkRemoveIcon from "@mui/icons-material/BookmarkRemove";
+import AlarmIcon from "@mui/icons-material/Alarm";
+import Autocomplete from "@mui/material/Autocomplete";
+import PaidIcon from "@mui/icons-material/Paid";
 import BookmarkIcon from "@mui/icons-material/Bookmark";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../context/authContext";
+import Accordion from "@mui/material/Accordion";
+import DatePicker from "react-datepicker";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
 
 const Feed = () => {
   const { currentUser } = useContext(AuthContext);
@@ -31,10 +44,14 @@ const Feed = () => {
   const [liked, setLiked] = useState(false);
   const [saved, setSaved] = useState(false);
   const [trigger, setTrigger] = useState(false);
+  const [allUsers, setAllUsers] = useState([]);
+  const [titleFilter, setTitleFilter] = useState("");
 
   const fetchData = async () => {
     try {
-      const res = await _axios.get(`jobs`);
+      const res = await _axios.get(
+        `jobs?title=${encodeURIComponent(titleFilter)}`
+      );
       setJobs(res.data);
       console.log(res.data);
     } catch (err) {
@@ -53,7 +70,8 @@ const Feed = () => {
   useEffect(() => {
     fetchData();
     getUser();
-  }, [trigger]);
+    getUsers();
+  }, [trigger, titleFilter]);
 
   const updateSavedJobs = async (jobId) => {
     try {
@@ -100,6 +118,16 @@ const Feed = () => {
       console.log(err);
     }
   };
+  const getUsers = async () => {
+    try {
+      const res = await _axios.get(`users/`);
+
+      setAllUsers(res.data);
+      console.log(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   const likeJob = async (jobId) => {
     try {
@@ -133,9 +161,174 @@ const Feed = () => {
     }
   };
 
+  ///////////////// SEARCH ///////////////////////////
+  const [selectedCountry, setSelectedCountry] = useState("");
+  const [yearFilter, setYearFilter] = useState("");
+  const [countries, setCountries] = useState([]);
+  const [selectedUser, setSelectedUser] = useState(null);
+  const [userFilter, setUserFilter] = useState("");
+  const [usernameFilter, setUsernameFilter] = useState("");
+  const handleCountryChange = (event, value) => {
+    setSelectedCountry(value || ""); // Set the selected country value or an empty string if no country is selected
+  };
+  const handleYearChange = (date) => {};
+  const handleClearYear = () => {
+    setYearFilter("");
+  };
+
+  const handleUserChange = (e, value) => {
+    if (value) {
+      setSelectedUser(value);
+      setUserFilter(value.id);
+      setUsernameFilter(value.username);
+    } else {
+      setSelectedUser(null);
+      setUserFilter("");
+      setUsernameFilter("");
+    }
+  };
+  const handleUsernameChange = (e) => {
+    setUsernameFilter(e.target.value);
+  };
+  const handleTitleChange = (e) => {
+    setTitleFilter(e.target.value);
+  };
+  //////////////////// SEARCH END  /////////////////////
+
+  const getGradientColors = (category) => {
+    const categoryColors = {
+      Technology: "from-blue-100 to-green-100",
+      Education: "from-purple-300 to-purple-100",
+      Marketing: "from-red-300 to-blue-50",
+      Design: "from-green-300 to-green-100",
+      Healthcare: "from-orange-300 to-orange-100",
+      Finance: "from-yellow-300 to-yellow-100",
+      Sales: "from-turquoise-300 to-turquoise-100",
+      Writing: "from-maroon-300 to-maroon-100",
+      Engineering: "from-teal-300 to-teal-100",
+      CustomerService: "from-rust-300 to-rust-100",
+      Administration: "from-gray-300 to-gray-100",
+      Science: "from-emerald-300 to-emerald-100",
+      Arts: "from-violet-300 to-violet-100",
+      HumanResources: "from-darkblue-300 to-darkblue-100",
+      Hospitality: "from-goldenrod-300 to-goldenrod-100",
+      Legal: "from-steelgray-300 to-steelgray-100",
+    };
+
+    return categoryColors[category] || "from-blue-50 to-gray-50"; // Default to gray if category not found
+  };
+
   return (
     <div className="flex flex-col items-center h-full bg-slate-100">
       <div className="text-2xl mb-4"></div>
+      <div>
+        <Accordion
+          sx={{ backgroundColor: "transparent", boxShadow: "none", margin: 0 }}
+          className="w-fit"
+        >
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+            className="w-fit"
+          >
+            <SearchIcon fontSize="large" className="hover:text-green-600" />
+          </AccordionSummary>
+          <AccordionDetails className="bg-white p-6 rounded-lg shadow-xl border-solid border-black">
+            Search Still under Development
+            <div className="flex items-center">
+              <div className="relative mr-4">
+                <input
+                  type="text"
+                  className="pl-8 pr-3 py-2 w-72 border rounded-md"
+                  placeholder="Search by title"
+                  value={titleFilter}
+                  onChange={handleTitleChange}
+                />
+                <SearchIcon className="w-4 h-4 absolute top-3 left-2 text-gray-400" />
+              </div>
+
+              <div className="relative mr-4">
+                <input
+                  type="text"
+                  className="pl-8 pr-3 py-2 w-72 border rounded-md"
+                  placeholder="Search by tags"
+                />
+                <TagIcon className="w-4 h-4 absolute top-3 left-2 text-gray-400" />
+              </div>
+              {allUsers ? (
+                <>
+                  <Autocomplete
+                    className="w-72 bg-white rounded-md"
+                    options={allUsers}
+                    getOptionLabel={(user) => user.username || ""}
+                    value={allUsers.find((option) => option.id === userFilter)}
+                    onChange={handleUserChange}
+                    onInputChange={handleUsernameChange}
+                    renderInput={(params) => (
+                      <TextField
+                        {...params}
+                        label={
+                          <div className="flex items-center">
+                            <PersonIcon />
+                            <span>Search by User</span>
+                          </div>
+                        }
+                        variant="outlined"
+                        InputProps={{
+                          ...params.InputProps,
+                          endAdornment: (
+                            <React.Fragment>
+                              {params.InputProps.endAdornment}
+                            </React.Fragment>
+                          ),
+                        }}
+                      />
+                    )}
+                  />
+                </>
+              ) : (
+                <></>
+              )}
+            </div>
+            <div className="flex items-center mr-4 ">
+             {/*  <div className="relative mb-5 mr-4 " style={{ zIndex: 9999 }}>
+                <DatePicker
+                  selected={yearFilter}
+                  onChange={(date) => handleYearChange(date)}
+                  dateFormat="yyyy"
+                  showYearPicker
+                  className="pl-8 pr-3 py-2 w-72 border rounded-md"
+                  placeholderText="Select a year"
+                  value={yearFilter ? yearFilter.toString() : ""}
+                />
+                <CalendarIcon className="w-4 h-4 absolute top-3 left-2 text-gray-400" />
+                {yearFilter && (
+                  <button
+                    className="absolute top-3 right-2 text-gray-400"
+                    onClick={handleClearYear}
+                  >
+                    Clear
+                  </button>
+                )}
+              </div>
+*/}
+              <Autocomplete
+                disablePortal
+                id="combo-box-demo"
+                options={countries}
+                sx={{ width: 250 }}
+                className="bg-white"
+                value={selectedCountry}
+                onChange={handleCountryChange}
+                renderInput={(params) => (
+                  <TextField {...params} label="Countries" />
+                )}
+              />
+            </div>
+          </AccordionDetails>
+        </Accordion>
+      </div>
 
       {jobs ? (
         <>
@@ -144,7 +337,9 @@ const Feed = () => {
               <>
                 {" "}
                 <div
-                  className=" transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-2xl mb-10 bg-gradient-to-b h-full text-gray-900 from-white to-blue-50  p-6 rounded-lg shadow-xl w-1/2 max-w-xl mx-auto mt-8 border-solid border-black"
+                  className={`transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-2xl mb-10 bg-gradient-to-b h-full text-gray-900 ${getGradientColors(
+                    job.category
+                  )}  p-6 rounded-lg shadow-xl w-1/2 max-w-xl mx-auto mt-8 border-solid border-black`}
                   style={{
                     fontFamily: "Montserrat, sans-serif",
                     boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
@@ -154,10 +349,15 @@ const Feed = () => {
                   {" "}
                   <Link to={`/job/${job._id}`}>
                     <div className="flex items-center justify-between mb-4">
-                      <h1 className="text-3xl font-medium">{job?.title}</h1>
-                      <div className="flex items-center space-x-2">
+                      <div className="w-3/5">
+                        <h1 className="text-3xl font-medium">{job?.title}</h1>
+                      </div>
+                      <div className="flex items-center space-x-2 w-2/5">
                         <img
-                          src="https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o="
+                          src={
+                            job?.user?.[0].profileImage ||
+                            "https://avatars.dicebear.com/api/adventurer-neutral/mail%40ashallendesign.co.uk.svg"
+                          }
                           alt="User Avatar"
                           style={{
                             width: "50px",
@@ -165,8 +365,13 @@ const Feed = () => {
                             borderRadius: "50%",
                           }}
                         />
-
-                        {job?.user?.[0]?.username || "Unknown"}
+                        <p className="text-sm">
+                          {job?.user?.[0]?.company || "Unknown"}
+                          <br />
+                          <p className="text-xs">
+                            Posted by : {job?.user?.[0]?.username || "Unknown"}
+                          </p>
+                        </p>
                       </div>
                     </div>
                     <div className="text-base font-medium mb-2">
@@ -178,14 +383,23 @@ const Feed = () => {
                         __html: job.description.slice(0, 90) + "...",
                       }}
                     ></div>
+                    <div className="flex text-sm items-center space-x-2">
+                      <PaidIcon className="text-green-800 mr-1" />
+                      {job.salary === "other"
+                        ? job.specificSalary + " $/year"
+                        : job.salary}
+                    </div>
                   </Link>
                   <div className="flex flex-wrap items-center space-x-4">
                     <div className="flex text-sm items-center space-x-2">
                       <LocationOnIcon />
                       {job?.location}
                     </div>
-                    <div className="flex text-sm items-center space-x-2">
-                      <AccessTimeIcon />
+                    <div
+                      className="flex text-sm items-center space-x-2 "
+                      style={{ textShadow: "0 0 5px white" }}
+                    >
+                      <AccessTimeIcon className="mr-1" />
                       {moment(job.postdate).fromNow()}
                     </div>
                     <div className="flex items-center text-sm space-x-2">
@@ -196,7 +410,7 @@ const Feed = () => {
                             <ThumbUpIcon
                               fontSize="large"
                               onClick={() => removeLike(job._id)}
-                              className=" cursor-pointer hover:text-red-800"
+                              className=" cursor-pointer hover:text-red-800 text-blue-800 mr-1"
                             />
                           </>
                         ) : (
@@ -204,7 +418,7 @@ const Feed = () => {
                             <ThumbUpOffAlt
                               fontSize="large"
                               onClick={() => likeJob(job._id)}
-                              className=" cursor-pointer hover:text-blue-800"
+                              className=" cursor-pointer hover:text-blue-800 text-blue-500"
                             />
                           </>
                         )
@@ -237,6 +451,10 @@ const Feed = () => {
                         />
                       </>
                     )}
+                    <div className="flex text-sm items-center space-x-2">
+                      <AlarmIcon className="text-red-800 mr-1" />
+                      {job?.deadline}
+                    </div>
                   </div>
                   <div className="mt-3">
                     <div className="text-lg font-semibold">Tags</div>
